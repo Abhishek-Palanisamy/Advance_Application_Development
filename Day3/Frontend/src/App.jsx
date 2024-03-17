9d6b9ce961b3a3c64137b6d79276d1400ebe6b3e
Abhishek P <110532245+Abhishek-Palanisamy@users.noreply.github.com>
2024-03-17 19:01:38 +0530

    Add files via upload

---
+++ Day3/Frontend/src/App.jsx
@@ -0,0 +1,50 @@
+import { Routes, Route } from "react-router-dom";
+import Profile from "./pages/users/Profile";
+import Contact from "./pages/Contact";
+import Register from "./pages/auth/Register";
+import { useEffect, useState } from "react";
+import Login from "./pages/auth/Login";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Personalized from "./pages/users/Personalized";
+import PersonalizedDetails from "./pages/users/PersonalizedDetails";
+
+function App() {
+  function userRoutes() {
+    return (
+      <Routes>
+        <Route path="/" element={<Profile />} />
+      </Routes>
+    );
+  }
+
+  function adminRoutes() {}
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    const token = sessionStorage.getItem("token");
+    if (token) {
+      const tokenexpiration = sessionStorage.getItem("tokenexpiration");
+      if (Date.now() < tokenexpiration) {
+        setIsLoggedIn(true);
+      }
+    }
+  });
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/personal" element={<Personalized />} />
+        <Route path="/personalized/:id" element={<PersonalizedDetails />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/user/*" element={userRoutes()} />
+        <Route path="/header" element={<Header/>} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;

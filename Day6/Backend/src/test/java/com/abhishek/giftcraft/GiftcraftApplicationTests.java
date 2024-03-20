abcf40076e6497c2f4d23eb89fac77b92ef3d7d5
Abhishek P <110532245+Abhishek-Palanisamy@users.noreply.github.com>
2024-03-20 14:47:52 +0530

    Add files via upload

---
+++ Day6/Backend/src/test/java/com/abhishek/giftcraft/GiftcraftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhishek.giftcraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftcraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
